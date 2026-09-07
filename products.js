411f62c60469d1745bfcf923d78e931051966c32
everythingbyivyofficial <everything.by.ivy.official@gmail.com>
2026-09-07 12:39:15 +0000

    products.js

--- products.js
+++ products.js
@@ -2,42 +2,82 @@ const products = [
 
 {
 id:1,
-name:"Luxury Pink Tote Bag",
-price:35000,
+name:"Luxury Red Handbag",
+price:30000,
 category:"Bags",
-image:"images/bag1.jpg"
+image:"images/red-bag.jpg"
 },
 
 {
 id:2,
-name:"Coach Burgundy Handbag",
-price:40000,
+name:"Pink Tote Bag",
+price:17000,
 category:"Bags",
-image:"images/bag2.jpg"
+image:"images/pink-tote.jpg"
 },
 
 {
 id:3,
-name:"Premium Black Heels",
-price:25000,
-category:"Heels",
-image:"images/heels1.jpg"
+name:"Pink Luxury Handbag",
+price:18000,
+category:"Bags",
+image:"images/pink-luxury.jpg"
 },
 
 {
 id:4,
-name:"Elegant Fashion Sandals",
-price:18000,
-category:"Sandals",
-image:"images/sandal1.jpg"
+name:"Coach Mini Bags",
+price:17000,
+category:"Bags",
+image:"images/coach-mini.jpg"
 },
 
 {
 id:5,
-name:"Luxury Pam Slippers",
-price:12000,
+name:"Black Strappy Heels",
+price:28000,
+category:"Heels",
+image:"images/black-heels.jpg"
+},
+
+{
+id:6,
+name:"Leopard Zara Heels",
+price:32000,
+category:"Heels",
+image:"images/leopard-heels.jpg"
+},
+
+{
+id:7,
+name:"Black Luxury Heels",
+price:28000,
+category:"Heels",
+image:"images/luxury-black-heels.jpg"
+},
+
+{
+id:8,
+name:"Designer Slippers",
+price:38000,
 category:"Slippers",
-image:"images/slipper1.jpg"
+image:"images/designer-slippers.jpg"
+},
+
+{
+id:9,
+name:"Brown Zara Heels",
+price:27000,
+category:"Heels",
+image:"images/brown-heels.jpg"
+},
+
+{
+id:10,
+name:"Gucci Shoulder Bag",
+price:22000,
+category:"Bags",
+image:"images/gucci-bag.jpg"
 }
 
 ];
