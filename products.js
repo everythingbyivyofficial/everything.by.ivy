546e0f3c8082ddf77896a783b6c91d8f174be0af
everythingbyivyofficial <everything.by.ivy.official@gmail.com>
2026-09-07 12:24:42 +0000

    products.js

---
+++ products.js
@@ -0,0 +1,43 @@
+const products = [
+
+{
+id:1,
+name:"Luxury Pink Tote Bag",
+price:35000,
+category:"Bags",
+image:"images/bag1.jpg"
+},
+
+{
+id:2,
+name:"Coach Burgundy Handbag",
+price:40000,
+category:"Bags",
+image:"images/bag2.jpg"
+},
+
+{
+id:3,
+name:"Premium Black Heels",
+price:25000,
+category:"Heels",
+image:"images/heels1.jpg"
+},
+
+{
+id:4,
+name:"Elegant Fashion Sandals",
+price:18000,
+category:"Sandals",
+image:"images/sandal1.jpg"
+},
+
+{
+id:5,
+name:"Luxury Pam Slippers",
+price:12000,
+category:"Slippers",
+image:"images/slipper1.jpg"
+}
+
+];
